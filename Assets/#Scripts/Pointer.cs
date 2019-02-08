c824a62f4510cd6a638bedacec5f1fee3fb2725a
이순형 <tnsgud9@naver.com>
2019-02-08 13:34:01 +0900

    Project Start

---
+++ Assets/#Scripts/Pointer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointer : MonoBehaviour
+{
+    public GameObject Point;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
