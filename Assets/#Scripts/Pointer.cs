47d200249f61cf37413cfe2fec7237f5e0893382
이순형 <tnsgud9@naver.com>
2019-02-23 03:13:09 +0900

    Code update

--- Assets/#Scripts/Pointer.cs
+++ Assets/#Scripts/Pointer.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Pointer : MonoBehaviour
 {
-    public GameObject Point;
+    public GameObject MousePoint;
+    private int _mask = 10;
     void Start()
     {
         
@@ -13,5 +14,15 @@ public class Pointer : MonoBehaviour
     void Update()
     {
         
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100f /*, 1<< LayerMask.NameToLayer("Background")*/ ))
+            {
+                    MousePoint.transform.position = hit.point;
+            }
+        }
     }
 }
