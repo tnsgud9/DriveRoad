59f40838047af6d5cca4c3924455bc67d09a051d
이순형 <tnsgud9@naver.com>
2019-02-28 13:01:58 +0900

    code optimization

--- Assets/#Scripts/Pointer.cs
+++ Assets/#Scripts/Pointer.cs
@@ -6,6 +6,9 @@ public class Pointer : MonoBehaviour
 {
     public GameObject MousePoint;
     private int _mask = 10;
+    
+    
+    
     void Start()
     {
         
@@ -13,15 +16,13 @@ public class Pointer : MonoBehaviour
 
     void Update()
     {
-        
-        if (Input.GetMouseButton(0))
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100f , 1<<11))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit, 100f /*, 1<< LayerMask.NameToLayer("Background")*/ ))
+            if (Input.GetMouseButton(0))
             {
-                    MousePoint.transform.position = hit.point;
+                MousePoint.transform.position = hit.point;
             }
         }
     }
